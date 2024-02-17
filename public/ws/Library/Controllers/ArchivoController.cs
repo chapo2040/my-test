492eac9b76da990b44c9ad402be2f36044788d9b
chapo2040 <chapo2040@hotmail.com>
2024-02-17 01:36:26 -0700

    123

--- public/ws/Library/Controllers/ArchivoController.cs
+++ public/ws/Library/Controllers/ArchivoController.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Collections;
-
-using Microsoft.AspNetCore.Mvc;
-using Library.Data;
-using Library.Models;
-using Microsoft.CodeAnalysis.Elfie.Model.Tree;
 using System.Reflection;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Model.Tree;
+
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+//using wsContaExpres;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.IO.Compression;
+using Microsoft.IdentityModel.Tokens;
+using ServiceReference1;
 
 namespace Library.Controllers
 {
@@ -16,6 +27,18 @@ namespace Library.Controllers
     public class ArchivoController : Controller
     {
         private readonly LibraryContext _context;
+
+        string urlAutentica = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion/Autenticacion.svc";
+        string urlAutenticaAction = "http://DescargaMasivaTerceros.gob.mx/IAutenticacion/Autentica";
+
+        string urlSolicitud = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/SolicitaDescargaService.svc";
+        string urlSolicitudAction = "http://DescargaMasivaTerceros.sat.gob.mx/ISolicitaDescargaService/SolicitaDescarga";
+
+        string urlVerificarSolicitud = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/VerificaSolicitudDescargaService.svc";
+        string urlVerificarSolicitudAction = "http://DescargaMasivaTerceros.sat.gob.mx/IVerificaSolicitudDescargaService/VerificaSolicitudDescarga";
+
+        string urlDescargarSolicitud = "https://cfdidescargamasiva.clouda.sat.gob.mx/DescargaMasivaService.svc";
+        string urlDescargarSolicitudAction = "http://DescargaMasivaTerceros.sat.gob.mx/IDescargaMasivaTercerosService/Descargar";
 
         public ArchivoController(LibraryContext context)
         {
@@ -29,7 +52,6 @@ namespace Library.Controllers
             try
             {
                 // HACER BUQUEDA DE ARCHIVOS EN CARPETA DEL HOSTING  FILE IO
-
                 string executingPath = Directory.GetCurrentDirectory();
                 string lsPath = executingPath + @"\facturas\";
                 int liContador = 1;
@@ -49,22 +71,17 @@ namespace Library.Controllers
                     }
                 }
 
-
                 //if (File.Exists(path))
                 //{
-                //    // This path is a file
                 //    ProcessFile(path);
                 //}
-
 
                 //string[] files = Directory.GetFiles(dir);
 
                 //for (int iFile = 0; iFile < files.Length; iFile++)
                 //    string fn = new FileInfo(files[iFile]).Name;
 
-
                 //string[] files = new DirectoryInfo(dir).GetFiles().Select(o => o.Name).ToArray();
-
 
                 return Ok(lstArchivos);
             }
@@ -74,67 +91,196 @@ namespace Library.Controllers
             }
         }
 
-
-        [HttpPost("Identificarse")]        
-        public IActionResult Identificarse()
+        [HttpPost("SatSolicitudDescarga")]
+        public IActionResult SatSolicitudDescarga()
         {
+            string RfcEmisor = "IATG9306278W9";
+            string RfcReceptor = "";
+            string FechaInicial = "2023-01-01";
+            string FechaFinal = "2023-10-30";
+
             try
             {
-                // CONECTARSE HACIENDA SAT
-                // cargar archivos de fiel.cer y llave.key
+                X509Certificate2 certificate = crearCertificado();
+                string autorizacion = this.SatAutenticacion(certificate);
 
+                if (autorizacion.IsNullOrEmpty() == false)
+                {
+                    // Solicitud de descarga masiva                
+                    Solicitud solicitud = new Solicitud(this.urlSolicitud, this.urlSolicitudAction);
+                    string xmlSolicitud = solicitud.Generate(certificate, RfcEmisor, RfcReceptor, RfcEmisor, FechaInicial, FechaFinal, "CFDI");
+                    string idSolicitud = solicitud.Send(autorizacion);
+                    Console.WriteLine("IdSolicitud: " + idSolicitud);
 
-                return Ok();
+                    return Ok(idSolicitud);
+                }
             }
             catch
             {
                 return BadRequest("Errors.");
+            }
+
+            return Ok();
+        }
+
+        private X509Certificate2 crearCertificado()
+        {
+            string gsCliente = "IATG9306278W9";
+            string gsFiel = "iatg9306278w9.cer";
+            string gsKey = "iatg9306278w9.key";
+            string gsPassword = "rentas01";
+
+            X509Certificate2 certificate;
+            byte[] pfx;
+            byte[] certBuffer = new byte[1024];
+            byte[] keyBuffer = new byte[1024];
+
+            string executingPath = Directory.GetCurrentDirectory();
+            string lsPath = executingPath + @"\certificados\";
+            string lsCertificadoPath = lsPath + gsCliente + @"\" + gsFiel;
+            string lsLlavePath = lsPath + gsCliente + @"\" + gsKey;
+
+            if (System.IO.File.Exists(lsCertificadoPath))
+            {
+                certBuffer = System.IO.File.ReadAllBytes(lsCertificadoPath);
+            }
+
+            if (System.IO.File.Exists(lsLlavePath))
+            {
+                keyBuffer = System.IO.File.ReadAllBytes(lsLlavePath);
+            }
+
+            pfx = CerKey2Pfx(certBuffer, keyBuffer, gsPassword);
+            certificate = new X509Certificate2(pfx, gsPassword);
+
+            return certificate;
+        }
+
+        private string SatAutenticacion(X509Certificate2 certificate)
+        {
+            try
+            {
+                // CONECTARSE HACIENDA SAT: cargar archivos de fiel.cer y llave.key
+                string token = ObtenerToken(certificate);
+                string autorization = String.Format("WRAP access_token=\"{0}\"", HttpUtility.UrlDecode(token));
+                Console.WriteLine("Token: " + token);
+                return autorization;
+            }
+            catch
+            {
+                return "";
             }
         }
 
-        [HttpPost("HacerPeticion")]
-        public IActionResult HacerPeticion()
+        private string ObtenerToken(X509Certificate2 cert)
+        {
+            Autenticacion service = new Autenticacion(this.urlAutentica, this.urlAutenticaAction);
+            string xml = service.Generate(cert);
+            return service.Send();
+        }
+
+        public static byte[] CerKey2Pfx(byte[] cerText, byte[] keyText, string keyPass)
         {
             try
             {
-                // Peticion
+                byte[] pfxBytes = null;
 
-                return Ok();
+                var certificado = new X509Certificate2(cerText);
+                var pllavePrivada = PrivateKeyFactory.DecryptKey(keyPass.ToCharArray(), keyText);
+
+                var certificadoBC = new Org.BouncyCastle.X509.X509Certificate(DotNetUtilities.FromX509Certificate(certificado).CertificateStructure);
+                var acertificadoBC = new X509CertificateEntry[1];
+                acertificadoBC[0] = new X509CertificateEntry(certificadoBC);
+
+                Pkcs12Store pfxStore = new Pkcs12StoreBuilder().Build();
+                pfxStore.SetKeyEntry("", new AsymmetricKeyEntry(pllavePrivada), acertificadoBC);
+                using (var pfxStream = new MemoryStream())
+                {
+                    pfxStore.Save(pfxStream, keyPass.ToCharArray(), new SecureRandom());
+                    pfxBytes = pfxStream.ToArray();
+                }
+
+                var resultado = Pkcs12Utilities.ConvertToDefiniteLength(pfxBytes, keyPass.ToCharArray());
+
+                return resultado;
             }
-            catch
+            catch (Exception ex)
             {
-                return BadRequest("Errors.");
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
             }
         }
 
-        [HttpPost("BajarArchivo")]
-        public IActionResult BajarArchivo()
+        [HttpPost("SatVerificacionDescarga")]
+        public IActionResult SatVerificacionDescarga()
         {
+            string RfcEmisor = "IATG9306278W9";
+            string idSolicitud = "640bc97e-fa4a-4bf4-b02b-171516608d5c";
+
             try
             {
-                // Checar y Bajar Archivo
+                X509Certificate2 certificate = crearCertificado();
+                string autorizacion = SatAutenticacion(certificate);
 
-                return Ok();
+                if (autorizacion.IsNullOrEmpty() == false)
+                {
+                    // Verificacion de Descarga Masiva
+                    VerificaSolicitud verifica = new VerificaSolicitud(this.urlVerificarSolicitud, this.urlVerificarSolicitudAction);
+                    string xmlVerifica = verifica.Generate(certificate, RfcEmisor, idSolicitud);
+                    string idPaquete = verifica.Send(autorizacion);
+
+                    if (idPaquete.IsNullOrEmpty() == false)
+                    {
+                        string response = this.SatDescargaMasiva(certificate, autorizacion, RfcEmisor, idPaquete);
+                        if (response.IsNullOrEmpty() == false) { GuardarSolicitud(idPaquete, response); }
+                    }
+
+                    return Ok(idPaquete);
+                }
             }
             catch
             {
                 return BadRequest("Errors.");
+            }
+
+            return Ok();
+        }
+
+        private string SatDescargaMasiva(X509Certificate2 certificate, string autorizacion, string rfcEmisor, string idPaquete)
+        {
+            try
+            {
+                // Bajar y Descomprimir Archivo ZIP
+                DescargarSolicitud descargarSolicitud = new DescargarSolicitud(this.urlDescargarSolicitud, this.urlDescargarSolicitudAction);
+                string xmlDescarga = descargarSolicitud.Generate(certificate, rfcEmisor, idPaquete);
+                string response = descargarSolicitud.Send(autorizacion);
+
+                return response;
+            }
+            catch
+            {
+                return "";
             }
         }
 
-        [HttpPost("DescomprimirArchivo")]
-        public IActionResult DescomprimirArchivo()
+        private void GuardarSolicitud(string idPaquete, string descargaResponse)
         {
-            try
-            {
-                // Descomprimir Archivo ZIP
+            string path = "./Paquetes/";
+            string filePath = path + idPaquete + ".zip";
+            string pathExtract = path + idPaquete + "/";
+            byte[] file = Convert.FromBase64String(descargaResponse);
 
-                return Ok();
-            }
-            catch
+            Directory.CreateDirectory(path);
+
+            using (FileStream fs = System.IO.File.Create(filePath, file.Length))
             {
-                return BadRequest("Errors.");
+                fs.Write(file, 0, file.Length);
             }
+
+            Console.WriteLine("FileCreated: " + filePath);
+
+            ZipFile.ExtractToDirectory(filePath, pathExtract);
+            Console.WriteLine("Unzip file: " + pathExtract);
         }
 
     }
